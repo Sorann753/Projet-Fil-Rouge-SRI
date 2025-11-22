0c444baaf0f5abd574083393977070fbf6ecbc22
GHOUILEM Abdelhafidh <111004606+hafidhghm24@users.noreply.github.com>
2025-11-22 14:25:51 +0100

    finalisation des fonction forward(), backward(), turn() dans le SimulatorController.c

--- source/actuator/SimulatorController.c
+++ source/actuator/SimulatorController.c
@@ -3,39 +3,42 @@
 #include <assert.h>
 #include <stdbool.h>
 
+/*chemin du fichier de sortie*/
+#define SIM_FILE "./SimulatorController.txt"
 
 /**
  * @brief procédure pour ecrire dans un fichier laction et la valeur
- * @param act laction enumerer
+ * @param act laction enumerer dans le .h
  * @param value la valuer associer a laction 
  */
 void WriteAction(action act, float value){ /*le char (pointeur = string) est en const (appliquer sur le char on ne le modifie pas dans la fonction) */
     
     /*ouvrire le fichier (mode a = ecrire a la fin du fichier)*/
-    FILE* my_file = fopen("./SimulatorControler.txt", "a" );
+    FILE* my_file = fopen(SIM_FILE, "a" );
     
     /*test de louverture du fichier*/
     if (!my_file){
-        printf("ERREUR: the SimulationControler hase note been able to open");
+        printf("SIM_FILE: the SimulationControler.txt hase note been able to open");
     }
+
 
     /*utilisation dun switch case pour ecrire en fonction des actions mise en entrer*/
     switch (act)
     {
     case ACT_FORWARD:
-        fprintf(my_file, "FORWARD %f", value);
+        fprintf(my_file, "FORWARD %f\n", value);
         break;
 
     case ACT_BACKWARD:
-        fprintf(my_file, "BACKWARD %f", value);
+        fprintf(my_file, "BACKWARD %f\n", value);
         break;
 
     case ACT_TURN:
-        fprintf(my_file, "TURN %f", value);
+        fprintf(my_file, "TURN %f\n", value);
         break;
 
     default:
-        fprintf(my_file, "ACTION %f", value);
+        fprintf(my_file, "ACTION %f\n", value);
         break;
     }
 
@@ -51,42 +54,49 @@ void WriteAction(action act, float value){ /*le char (pointeur = string) est en 
  * @param distance (en metre)
  */
 void forward(float distance){
-    assert(distance > 0);
-    // bool valid_value;
-    
-    // /*tester la validiter de la distance*/
-    // if (distance > 10000){
-    //     valid_value = false;
-    // }else valid_value = true;
-
+    if (distance<0){
+        fprintf(stderr, "forward(): distance must be >0 \n");
+        return;
+    }
+    assert(distance<0);
 
     /*apelle de la fonction pour ecrire dans le SimulatorControler.txt pour pouvoir communiquer avec le python*/
     WriteAction(ACT_FORWARD, distance);
-
 } 
+
 
 
 /**
  * @brief procédure qui utilise forward() en changent le signe 
  * @param distance
  */
-// void backward(float distance){
+void backward(float distance){
+    if (distance<0){
+        fprintf(stderr, "backward(): distance must be >0 \n");
+        return;
+    }
+    assert(distance<0);
 
+    /*apelle de la fonction pour ecrire dans le SimulatorControler.txt pour pouvoir communiquer avec le python*/
+    WriteAction(ACT_BACKWARD, distance);
 
-// }
+}
 
 
 
 /**
  * @brief procédure pour tourner d'un angle en simulation
- * @param angle (en degrer)
+ * @param angle (en degrer entre -360 et 360)
  */
-// void turn(float angle){
+void turn(float angle){
+    if(angle>360 || angle<(-360)){
+        fprintf(stderr, "turn(): angle value must be between -360 and +360 degr \n");
+        return;
+    }
+    /*apelle de la fonction pour ecrire dans le SimulatorControler.txt pour pouvoir communiquer avec le python*/
+    WriteAction(ACT_TURN, angle);
 
-
-
-
-// }
+}
 
 
 
