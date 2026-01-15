b5e368cffe29e3f272269d8ede04fcc8ad61259b
Hafidhghm24 <hafidhghouilem@gmail.com>
2026-01-15 11:11:19 +0100

    correction de la connexion google

--- source/manualPilot/cutter.c
+++ source/manualPilot/cutter.c
@@ -15,4 +15,30 @@ tokenlist init_tokenizer()
  */
 tokenlist tokenizer(char *phrase, tokenlist *result)
 {
+    /**
+     * strtok(phrase, " ") : decouper la phrase (replace " " par \0)
+     * strncpy(prase1, phrase2, nb_char_limite) : copier une phrase
+     */
+
+    // copier la phrase
+    char temp[];
+    strncpy(phrase, temp);
+
+    // decouper la phrase
+    strtok(temp, " ");
+
+    // mettre les mots dans tokenlist
+    int i = 0;
+
+    for (int j; i < len(temp), j++)
+    {
+        while (temp[i] != "\0")
+        {
+            char mot[i] = temp[i]
+
+                i++;
+        }
+        result->tokenTAB
+            [j] = mot;
+    }
 }
