7edc27034db3df07e3106735863ae0ae1f8e9b93
Hafidhghm24 <hafidhghouilem@gmail.com>
2026-01-16 22:56:25 +0100

    commencer le parser.c et parser.h (ça compile !)
    fonciton d'initialisation de la liste de commande
    
    fonction d'initialisation de la commande
    
    fonction parser() qui parcours la liste de token et quand on croise un verbe on regarde la token dapres (si cest une valeur alors on stoke la valeur dans la commande cmd.data.value)(si cest une couleur on stok la couleur dans cmd.data.color ) et retourne le nombre de commande de la liste de commande

--- include/manualPilot/parser.h
+++ include/manualPilot/parser.h
@@ -1,7 +1,26 @@
+/**
+ * @author GHOUILEM ABDELHAFIDH
+ */
 #ifndef __PARSER_H__
 #define __PARSER_H__
 
+#include "manualPilot/command.h"
+#include "manualPilot/cutter.h"
+
 /**
- * TODO: construit les commandes
+ * @brief Initialise une liste de commandes vide
  */
+command_list init_command_list(void);
+
+/**
+ * @brief initialise une commande vide
+ */
+static void init_command(command_list *cmd_list);
+
+/**
+ * @brief transformer la liste de tokens en commandes
+ * @param len_command_list retourne la taille de la liste de commande
+ */
+int parser(tokenlist *token_list, command_list *cmd_list);
+
 #endif
---
+++ source/manualPilot/parser.c
@@ -0,0 +1,86 @@
+/**
+ * @author GHOUILEM ABDELHAFIDH
+ */
+#include <stdio.h>
+#include <stdlib.h>
+#include "manualPilot/parser.h"
+
+/**
+ * @brief Initialise une liste de commandes vide
+ */
+command_list init_command_list(void)
+{
+    command_list list;
+    list.count = 0;
+    return list;
+}
+
+/**
+ * @brief initialise une commande vide
+ */
+static void init_command(command_list *cmd_list)
+{
+    cmd_list->cmd[cmd_list->count].action = ACT_NONE;
+    cmd_list->cmd[cmd_list->count].value = 0.0;
+    cmd_list->cmd[cmd_list->count].color = COL_NONE;
+    cmd_list->cmd[cmd_list->count].object = OBJ_NONE;
+    cmd_list->cmd[cmd_list->count].direction = DIR_NONE;
+    cmd_list->cmd[cmd_list->count].negated = 0;
+}
+
+/**
+ * @brief transformer la liste de tokens en commandes
+ */
+int parser(tokenlist *token_list, command_list *cmd_list)
+{
+    if (token_list == NULL || cmd_list == NULL)
+    {
+        fprintf(stderr, "parser: ERREUR pointeur null\n");
+        return -1;
+    }
+
+    /*initialiser le compteur*/
+    cmd_list->count = 0;
+
+    int i = 0;
+    while (i < token_list->count && cmd_list->count < MAX_COMMAND)
+    {
+        if (token_list->tokenTAB[i].type == TOK_VERBE)
+        {
+            /* initialiser la commande */
+            init_command(cmd_list);
+
+            /* récupération de l'action */
+            cmd_list->cmd[cmd_list->count].action = token_list->tokenTAB[i].data.action;
+
+            /*parcourir toute la liste de token jusquau prochain verbe*/
+            int j = i + 1;
+            while (j < token_list->count && token_list->tokenTAB[j].type != TOK_VERBE)
+            {
+                /* si c'est un nombre */
+                if (token_list->tokenTAB[j].type == TOK_NUM)
+                {
+                    /*remplacer la valeur de la commande par la valeur du token*/
+                    cmd_list->cmd[cmd_list->count].value = token_list->tokenTAB[j].data.value;
+                }
+
+                /* si c'est une couleur */
+                if (token_list->tokenTAB[j].type == TOK_COLOR)
+                {
+                    cmd_list->cmd[cmd_list->count].color = token_list->tokenTAB[j].data.color;
+                }
+            }
+
+            cmd_list->count++;
+            i = j; /*on continue a partir de i+1*/
+        }
+        /*si ce nest pas un verbe on passe a la suivante*/
+        else
+        {
+            i++;
+        }
+    }
+
+    /*retourner le nombre de commande dans la liste de commande*/
+    return cmd_list->count;
+}
