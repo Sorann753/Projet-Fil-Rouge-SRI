8440cc847307327ea67e816a4a2c167e44dab31f
Hafidhghm24 <hafidhghouilem@gmail.com>
2026-04-14 13:38:41 +0200

    ajout de la fonction enovoie_commande()

--- controller/PlatformIO/include/constants.hpp
+++ controller/PlatformIO/include/constants.hpp
@@ -10,5 +10,6 @@ const long timeoutPi = 2000;
 /* ODOMETRIE*/
 const float CM_PAR_SECONDE = 30.0;  // A mesurer physiquement : Vitesse du robot (cm/s) a PWM 200
 const float DEG_PAR_SECONDE = 90.0; // A mesurer physiquement : Vitesse de rotation (deg/s) a PWM 200
+const float DIST_SECUR = 40.0;
 
 #endif
