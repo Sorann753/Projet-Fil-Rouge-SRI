c95d9bf0c438d8d67ca8116f606fca04dcbd8476
kaedesword <59872253+demoryl@users.noreply.github.com>
2026-04-14 13:51:29 +0200

    changement syntaxe command.cpp

--- controller/PlatformIO/src/updater/command.cpp
+++ controller/PlatformIO/src/updater/command.cpp
@@ -1,15 +1,27 @@
 #include "updater/command.hpp"
+#include "constants.hpp"
 
 Command currentCmd = {"", 0, false};
 
 void readCommand() {
-    if (Serial.available() > 0) {
-        String input = Serial.readStringUntil('\n');
-        input.trim();
+  if (Serial.available() > 0) {
+    extern unsigned long dernierMessagePi;
+    dernierMessagePi = millis();
 
-        int sep = input.indexOf(':');
-        currentCmd.action = (sep != -1) ? input.substring(0, sep) : input;
-        currentCmd.valeur = (sep != -1) ? input.substring(sep + 1).toInt() : 0;
-        currentCmd.active = true;
+    String cmd = Serial.readStringUntil('\n');
+    cmd.trim();
+
+    int index_separateur = cmd.indexOf(':');
+    String action = cmd;
+    int valeur = 0;
+
+    if (index_separateur != -1) {
+      action = cmd.substring(0, index_separateur);
+      valeur = cmd.substring(index_separateur + 1).toInt();
     }
+    
+    currentCmd.action = action;
+    currentCmd.valeur = valeur;
+    currentCmd.active = true;
+  }
 }
