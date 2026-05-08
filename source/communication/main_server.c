e8324682f455230000aaa61a035251e26c15c57a
Hafidhghm24 <hafidhghouilem@gmail.com>
2026-05-08 20:16:40 +0200

    separer le code main_server.c en fonction et ajout de documentation  detaillé

--- source/communication/main_server.c
+++ source/communication/main_server.c
@@ -9,7 +9,9 @@
 enum MODE mode = MANUEL; // 0 = MANUEL (par défaut), 1 = AUTONOME
 
 /**
- * @brief envoi des données de telemetrie au GUI
+ * @brief envoi des données de telemetrie au GUI en trame (DATA;...)
+ * @param data Structure contenant l'état actuel, la commande, la distance et le temps restant.
+ *
  */
 void envoieTelemetrie(RobotData data, int client_socket)
 {
@@ -35,11 +37,115 @@ void modeAutonome(RobotData *data)
             // Evitement dobstacle
             sendCommand("B", 30); // recule
             sendCommand("R", 90); // tourne
-            printf("[AUTONOME] Obstacle à %d cm ! Recul puis rotation gauche.\n", data->distAv);
+            printf("[AUTONOME] Obstacle à %d cm ! Recul puis rotation droite.\n", data->distAv);
         }
 
         // modifie la vraie variable du main grâce au pointeur
         data->state = 1;
+    }
+}
+
+/**
+ * @brief traitement de la chaîne TCP reçue du GUI
+ * Intercepte les commandes de securité (S) et changement de mode (M)
+ * en mode MANUEL : envoie les ordres de mouvement (F, B, R, L) à l'Arduino
+ * @param buffer La chaîne de caractères brute reçue sur le port 5000 en TCP
+ */
+void traitementCommande(char *buffer)
+{
+    buffer[strcspn(buffer, "\r\n")] = '\0';
+
+    printf("\n[GUI DIT] : %s\n", buffer);
+    fflush(stdout);
+
+    char action[10];
+    int valeur = 0;
+
+    // parsing du buffer recu par le GUI
+    if (sscanf(buffer, "%s %d", action, &valeur) >= 1)
+    {
+        // si on recoi un "S"
+        if (strcmp(action, "S") == 0)
+        {
+            mode = MANUEL;       // forcer le passage en mode manuelle
+            sendCommand("S", 0); // envoie dun stop
+            printf("[STOP] Arrêt d'urgence et retour MANUEL.\n");
+        }
+        // si on recoie un "M"
+        else if (strcmp(action, "M") == 0)
+        {
+            // mode autonome
+            if (valeur == 1)
+            {
+                mode = AUTONOME;
+                printf("[AUTONOME] mode autonome activé\n");
+            }
+            // mode manuel
+            else
+            {
+                mode = MANUEL;
+                printf("[MANUEL] mode manuel activé\n");
+            }
+            sendCommand("S", 0); // on stop lors du changement de mode
+        }
+        else if (mode == MANUEL)
+        {
+            sendCommand(action, valeur);
+            printf("[UART -> ARDUINO] : %s %d\n", action, valeur);
+        }
+        else
+        {
+            printf("[AUTONOME] commande Ignoré : Le robot est en mode AUTONOME.\n");
+        }
+    }
+}
+
+/**
+ * @brief ecoute le retour (ARDUINO => RBI) et rempli la structure data
+ * Détecte les fins de mouvements (Transition 1 -> 0), affiche les logs critiques (Obstacles)
+ * transfert des données vers le GUI.
+ */
+void ecouteArduino(RobotData *data, int client_socket)
+{
+    char *msg = readArduino();
+    if (msg != NULL && parseTelemetry(msg, data))
+    {
+
+        // mémoriser l'état au tour de boucle précédent
+        static int etat_precedent = 0;
+
+        // Le robot vient de s'arrêter
+        if (etat_precedent == 1 && data->state == 0)
+        {
+            printf("[PIPELINE] Mouvement '%s' termine avec succes. Robot disponible.\n", data->cmd);
+        }
+
+        // Mouvement ou Danger
+        if (data->state == 1 || data->distAv <= 20)
+        {
+            printf("[ARDUINO -> PI] Etat: %d | Action: %s | Obstacle: %d cm\n",
+                   data->state, data->cmd, data->distAv);
+        }
+
+        // Mise à jour de la mémoire
+        etat_precedent = data->state;
+
+        // ENVOIE des data au GUI
+        envoieTelemetrie(*data, client_socket);
+    }
+}
+
+/**
+ * @brief envoie un PING tout les 50ms pour reinitialisation du watchdog arduino
+ */
+void heartbeat()
+{
+    static int heartbeat_counter = 0;
+    heartbeat_counter++;
+    if (heartbeat_counter >= 5) // MODIFICATION: 5 au lieu de 20
+    {
+        sendCommand("PING", 0);
+        heartbeat_counter = 0;
     }
 }
 
@@ -90,89 +196,14 @@ int main()
             else if (octetslus > 0)
             {
                 // On a reçu une vraie commande on la traite
-                buffer[strcspn(buffer, "\r\n")] = '\0';
-
-                printf("\n[GUI DIT] : %s\n", buffer);
-                fflush(stdout);
-
-                char action[10];
-                int valeur = 0;
-
-                // parsing du buffer recu par le GUI
-                if (sscanf(buffer, "%s %d", action, &valeur) >= 1)
-                {
-                    // si on recoi un "S"
-                    if (strcmp(action, "S") == 0)
-                    {
-                        mode = MANUEL;       // forcer le passage en mode manuelle
-                        sendCommand("S", 0); // envoie dun stop
-                        printf("[STOP] Arrêt d'urgence et retour MANUEL.\n");
-                    }
-                    // si on recoie un "M"
-                    else if (strcmp(action, "M") == 0)
-                    {
-                        // mode autonome
-                        if (valeur == 1)
-                        {
-                            mode = AUTONOME;
-                            printf("[AUTONOME] mode autonome activé\n");
-                        }
-                        // mode manuel
-                        else
-                        {
-                            mode = MANUEL;
-                            printf("[MANUEL] mode manuel activé\n");
-                        }
-                        sendCommand("S", 0); // on stop lors du changement de mode
-                    }
-                    else if (mode == MANUEL)
-                    {
-                        sendCommand(action, valeur);
-                        printf("[UART -> ARDUINO] : %s %d\n", action, valeur);
-                    }
-                    else
-                    {
-                        printf("[AUTONOME] commande Ignoré : Le robot est en mode AUTONOME.\n");
-                    }
-                }
+                traitementCommande(buffer);
             }
 
             // ECOUTE ARDUINO (on lit à chaque tour)
-            char *msg = readArduino();
-            if (msg != NULL && parseTelemetry(msg, &data))
-            {
-
-                // mémoriser l'état au tour de boucle précédent
-                static int etat_precedent = 0;
-
-                // Le robot vient de s'arrêter
-                if (etat_precedent == 1 && data.state == 0)
-                {
-                    printf("[PIPELINE] Mouvement '%s' termine avec succes. Robot disponible.\n", data.cmd);
-                }
-
-                // Mouvement ou Danger
-                if (data.state == 1 || data.distAv <= 20)
-                {
-                    printf("[ARDUINO -> PI] Etat: %d | Action: %s | Obstacle: %d cm\n",
-                           data.state, data.cmd, data.distAv);
-                }
-
-                // Mise à jour de la mémoire
-                etat_precedent = data.state;
-
-                // ENVOIE des data au GUI
-                envoieTelemetrie(data, client_socket);
-            }
+            ecouteArduino(&data, client_socket);
 
             // securite batement de coeur de l'arduino
-            static int heartbeat_counter = 0;
-            heartbeat_counter++;
-            if (heartbeat_counter >= 5) // MODIFICATION: 5 au lieu de 20
-            {
-                sendCommand("PING", 0);
-                heartbeat_counter = 0;
-            }
+            heartbeat();
 
             // DECISION AUTONOME
             modeAutonome(&data);
