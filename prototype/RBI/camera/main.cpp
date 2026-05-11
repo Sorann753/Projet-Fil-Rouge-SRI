6af9ca0a8c9bad422d686353743af35472266d56
Sorann753 <Arthusd@hotmail.fr>
2026-05-11 08:41:00 +0200

    update camera module (almost done)
    - i'm pushing mostly for other to sync up, but i'm not done yet

--- prototype/RBI/camera/main.cpp
+++ prototype/RBI/camera/main.cpp
@@ -4,14 +4,115 @@
 #include <chrono>
 #include <thread>
 
+#include <sys/mman.h>
+
 #include "libcamera/libcamera.h"
+
+#include "pfr-core/include/vision/image.h"
 
 static std::shared_ptr<libcamera::Camera> camera;
 
+/**
+ * @brief RAII class to manage the mapped page
+ */
+struct MappedImgBuffer{
+    MappedImgBuffer(int fd, std::size_t sz) : _size(sz)
+    {
+        data = mmap(NULL, sz, PROT_READ, MAP_SHARED, fd, 0);
+        isValid = (data != MAP_FAILED);
+    }
+    ~MappedImgBuffer(){
+        if(data != MAP_FAILED){
+            munmap(data, _size);
+        }
+    }
+
+    // move constructors
+    MappedImgBuffer(MappedImgBuffer&& other) noexcept
+    : data(other.data), _size(other._size)
+    {
+        other.data = nullptr; //we stole the data
+        other._size = 0;
+    }
+
+    MappedImgBuffer& operator=(MappedImgBuffer&& other) noexcept {
+        if(this != &other){
+            if(data && data != MAP_FAILED){
+                munmap(data, _size);
+            }
+            data = other.data;
+            other.data = nullptr;
+
+            _size = other._size;
+            other._size = 0;
+        }
+        return *this;
+    }
+
+    // forbid the copy
+    MappedImgBuffer(const MappedImgBuffer&) = delete;
+    MappedImgBuffer& operator=(const MappedImgBuffer&) = delete;
+
+    const uint8_t* getPixels() const {
+        return static_cast<uint8_t*>(data);
+    }
+
+    bool isValid = false;
+    void* data;
+    std::size_t _size;
+};
+
+/**
+ * @brief RAII class to manage the image ressource
+ */
+struct ImageOwner{
+    ImageOwner() : img({nullptr, 0, 0, false})
+    {}
+
+    ImageOwner(std::size_t height, std::size_t width) : img(initMatrix(height, width, sizeof(Pixel)))
+    {
+        isValid = img.isValid;
+    }
+    ~ImageOwner(){
+        freeMatrix(&img);
+    }
+
+    /**
+     * @brief allow us to get pixels with simple image(x, y) access
+     */
+    Pixel& operator()(const std::size_t X, const std::size_t Y){
+        Pixel* px = static_cast<Pixel*>(img.content);
+        auto id = matrixGetIndex(&img, X, Y);
+        return px[id];
+    }
+
+    Image img;
+    bool isValid = false;
+};
+
+ImageOwner BufferToImage(const MappedImgBuffer& dataBuffer, std::size_t width, std::size_t heigh, std::size_t stride)
+{
+    // pixels is a 3D matrix here
+    const uint8_t* pixels = dataBuffer.getPixels();
+    ImageOwner newImage(heigh, width);
+
+    // we're pretty much forced to make a copy there to have the proper type
+    for(auto x = 0; x < width; x++){
+        for(auto y = 0; y < heigh; y++){
+            std::size_t id = (y * stride) + (x * 3);
+
+            newImage(x, y).R = pixels[id + 0];
+            newImage(x, y).G = pixels[id + 1];
+            newImage(x, y).B = pixels[id + 2];
+        }
+    }
+
+    return newImage;
+}
+
 class RpiCamera{
 public:
-    RpiCamera() : cm{std::make_unique<libcamera::CameraManager>()}
-    {
+    RpiCamera() {
         using namespace libcamera;
         cm->start();
 
@@ -66,7 +167,7 @@ public:
         stream = streamConfig.stream();
         const std::vector<std::unique_ptr<FrameBuffer>> &buffers = allocator->buffers(stream);
     
-        for (unsigned int i = 0; i < buffers.size(); ++i) {
+        for (std::size_t i = 0; i < buffers.size(); i++) {
             std::unique_ptr<Request> request = camera->createRequest();
             if (!request)
             {
@@ -108,46 +209,47 @@ public:
         cm->stop();
     }
 
-    int requestImage(){
-        //TODO
-        return 0;
+    ImageOwner consumeImage(){
+        if(!imgReady.isValid){
+            // return an empty image because there is nothing to get
+            return ImageOwner();
+        }
+
+        ImageOwner img = BufferToImage(imgReady, readyConf.size.width, readyConf.size.height, readyConf.stride);
+        return img;
     }
 
     static void requestComplete(libcamera::Request* request){
-        std::cout << "!!!" << std::endl;
+        std::cout << "[!!!]" << std::endl;
         using namespace libcamera;
         if (request->status() == Request::RequestCancelled){
-           return;
+            return;
         }
 
-        const std::map<const Stream*, FrameBuffer*>& buffers = request->buffers();
-        for (auto bufferPair : buffers) {
-            // actually process the images
+        // extract the buffer
+        auto [stream, buffer] = *(request->buffers().begin());
+        int fd = buffer->planes()[0].fd.get();
+        std::size_t size = buffer->planes()[0].length;
 
-            FrameBuffer *buffer = bufferPair.second;
-            const FrameMetadata &metadata = buffer->metadata();
-        
-            std::cout << " seq: " << std::setw(6) << std::setfill('0') << metadata.sequence << " bytesused: ";
+        MappedImgBuffer content(fd, size);
+        if(!content.isValid()) return;
 
-            unsigned int nplane = 0;
-            for (const FrameMetadata::Plane &plane : metadata.planes())
-            {
-                std::cout << plane.bytesused;
-                if (++nplane < metadata.planes().size()) std::cout << "/";
-            }
-            
-            std::cout << std::endl;
-        } 
+        readyConf = stream->configuration();
 
-        //request->reuse(Request::ReuseBuffers);
-        //camera->queueRequest(request);
+        // keep the camera going
+        request->reuse(Request::ReuseBuffers);
+        camera->queueRequest(request);
     }
 
 private:
-    std::unique_ptr<libcamera::CameraManager> cm;
+    std::unique_ptr<libcamera::CameraManager> cm = std::make_unique<libcamera::CameraManager>();
     std::unique_ptr<libcamera::FrameBufferAllocator> allocator;
     libcamera::Stream* stream = nullptr;
     std::vector<std::unique_ptr<libcamera::Request>> requests;
+
+    //TODO : make thread safe
+    static MappedImgBuffer imgReady;
+    static libcamera::StreamConfiguration readyConf;
 
     bool isValid = false;
 };
@@ -158,7 +260,7 @@ int main(){
     using namespace std::chrono_literals;
     RpiCamera rpicam;
 
-    int img = rpicam.requestImage();
+    ImageOwner img = rpicam.consumeImage();
     std::cout << "a mimir" << std::endl;
     std::this_thread::sleep_for(3000ms);   
     std::cout << "end" << std::endl;
